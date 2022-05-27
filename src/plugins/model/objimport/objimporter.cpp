8a3e3fbf7bd60c9d2461d590d69909f5f5a54c17
Nikita Prianichnikov <lpenguin@yandex.ru>
2022-05-27 14:53:02 +0300

    ObjectImport: various fixes

--- src/plugins/model/objimport/objimporter.cpp
+++ src/plugins/model/objimport/objimporter.cpp
@@ -1,16 +1,17 @@
 #include "objimporter.h"
 #include <QSet>
 #include <volint/volint.h>
+#include <mtool/ModelClass.h>
 #include <QDebug>
 
 using namespace vangers::model::objimport;
 
-struct Bounds;
-
-void makeBoundingBox(Object& result, const Bounds& bounds);
+void makeBoundingBox(Object& result,const Size3F64& bounds);
 void skinBodyBounds(const Object& body, Object& bounds);
 void alterBoundingBox(C3D& bounds, C3D& body, bool isMechous);
 void calcPhysics(C3D& c3d);
+void calcPhysicsMtool(C3D& c3d);
+
 //QList<Face> triangulate(const Face& face){
 //	if(face.indices.size() == 3){
 //		return {face};
@@ -27,13 +28,64 @@ void calcPhysics(C3D& c3d);
 //	return result;
 //}
 
+
+void calcPhysicsMtool(C3D& c3d){
+	mtool::Vertex* vertices = new mtool::Vertex[c3d.vectices.size()];
+	mtool::Face* faces = new mtool::Face[c3d.polygons.size()];
+
+	for(size_t i = 0; i < c3d.vectices.size(); i++){
+		const Vector3F32& v = c3d.vectices[i].tf;
+		vertices[i][0] = v.x;
+		vertices[i][1] = v.y;
+		vertices[i][2] = v.z;
 	}
 
+	for(size_t i = 0; i < c3d.polygons.size(); i++){
+		const Polygon& p = c3d.polygons[i];
+		if(p.num != 3) {
+			faces[i] = mtool::Face{};
+		} else {
+			faces[i] = mtool::Face {
+				.Vertices = {
+					(uint32_t) p.indices[0].vertInd,
+					(uint32_t) p.indices[1].vertInd,
+					(uint32_t) p.indices[2].vertInd
+				},
+	//			.Normals = {
+	//				(uint32_t) p.indices[0].normInd,
+	//				(uint32_t) p.indices[1].normInd,
+	//				(uint32_t) p.indices[2].normInd
+	//			}
+			};
+		}
 
 
+	}
 
+	mtool::Model model;
+	model.VerticesCount = c3d.vectices.size();
+	model.Vertices = vertices;
+	model.FacesCount = c3d.polygons.size();
+	model.Faces = faces;
 
+	mtool::PhysicalParamsStruct pps {};
 
+	model.MakeFacesNormals();
+//	model.MakeVerticesNormals();
+	model.CalculateDims();
+	model.CalculatePhysicalParams(pps, false);
+	c3d.volume = pps.Volume;
+	c3d.rcm.x = pps.MassCenter[0];
+	c3d.rcm.y = pps.MassCenter[1];
+	c3d.rcm.z = pps.MassCenter[2];
+
+//	Vector3F64 geomCenter = Vector3F64::fromVector((c3d.minBound + c3d.maxBound) / 2.0f);
+//	c3d.rcm = c3d.rcm + geomCenter;
+
+	for(int i = 0; i < 9; i++){
+		c3d.J.values[i] = pps.IntertiaTensor[i];
+	}
+}
 
 void calcPhysics(C3D& c3d){
 	double(* verts)[3] = new double[c3d.vectices.size()][3];
@@ -130,43 +182,43 @@ void objectToC3D(
 			colorId = importSettings.materialsMapping[group.material];
 		}
 
-				Polygon polygon {
-					.num = face.indices.size(),
-					.colorId = (uint32_t)colorId,
-				};
-
-				for(const FaceIndex& index: face.indices){
-					polygon.indices.push_back({
-						.vertInd = index.vertexIndex,
-						.normInd = index.normalIndex,
-					});
-				}
-				std::reverse(polygon.indices.begin(), polygon.indices.end());
 		for(const Face& face: group.faces){
+			Polygon polygon {
+				.num = face.indices.size(),
+				.colorId = (uint32_t)colorId,
+			};
 
-				const Vector3F32& v1 = vertices[polygon.indices[0].vertInd].tf;
-				const Vector3F32& v2 = vertices[polygon.indices[1].vertInd].tf;
-				const Vector3F32& v3 = vertices[polygon.indices[2].vertInd].tf;
-
-				Vector3F32 norm = (v2 - v1).cross(v2 - v3).normalized();
-				Normal flatNormal = {
-					.normal = (Vector3I8)(norm * 123),
-					.nPower = 8
-				};
-
-				Vector3F64 middle = {};
-				for(const PolygonIndex& index: polygon.indices) {
-					middle = middle + vertices[index.vertInd].tf;
-				}
-				middle = middle / 4;
-
-				polygon.colorId = (uint32_t)colorId;
-				polygon.flatNormal = flatNormal;
-				polygon.middle = middle;
-
-				polygons.push_back(polygon);
+			for(const FaceIndex& index: face.indices){
+				polygon.indices.push_back({
+					.vertInd = index.vertexIndex,
+					.normInd = index.normalIndex,
+				});
 			}
+			std::reverse(polygon.indices.begin(), polygon.indices.end());
 
+			const Vector3F32& v1 = vertices[polygon.indices[0].vertInd].tf;
+			const Vector3F32& v2 = vertices[polygon.indices[1].vertInd].tf;
+			const Vector3F32& v3 = vertices[polygon.indices[2].vertInd].tf;
+
+			Vector3F32 norm = (v1 - v2).cross(v3 - v1).normalized();
+			Vector3F32 normScaled = norm * 123.0f;
+
+			Normal flatNormal = {
+				.normal = Vector3I8::round(normScaled),
+				.nPower = 0
+			};
+
+			Vector3F32 middle = {};
+			for(const PolygonIndex& index: polygon.indices) {
+				middle = middle + vertices[index.vertInd].tf;
+			}
+			middle = middle / polygon.indices.size();
+
+			polygon.colorId = (uint32_t)colorId;
+			polygon.flatNormal = flatNormal;
+			polygon.middle = Vector3I8::round(middle);
+
+			polygons.push_back(polygon);
 		}
 
 	}
@@ -187,11 +239,11 @@ void objectToC3D(
 	c3d.numNorm = (int32_t)normals.size();
 	c3d.numPoly = (int32_t)polygons.size();
 	c3d.numVertTotal = numVertTotal;
-	c3d.maxBound = bounds.max * scaleFactor;
-	c3d.minBound = bounds.min * scaleFactor;
+	c3d.maxBound = Vector3I32::round(bounds.max * scaleFactor);
+	c3d.minBound = Vector3I32::round(bounds.min * scaleFactor);
 	c3d.angle = {83, 83, 83};
-	c3d.rmax = maxLength;
-	c3d.offsets = origin;
+	c3d.rmax = qRound(maxLength);
+	c3d.offsets = {0, 0, 0};
 	c3d.vectices = vertices;
 	c3d.normals = normals;
 	c3d.polygons = polygons;
@@ -201,26 +253,6 @@ void objectToC3D(
 	c3d.J = Matrix3x3F64::identity();
 	c3d.rcm = {};
 	c3d.volume = 0;
-}
-
-Bounds objectBounds(const Object& obj){
-	if(obj.vertices.size() == 0) {
-		return {};
-	}
-
-	Vector3F64 minPos = obj.vertices[0];
-	Vector3F64 maxPos = obj.vertices[1];
-
-	for(const Vector3F64& v: obj.vertices){
-		minPos.x = std::min(v.x, minPos.x);
-		minPos.y = std::min(v.y, minPos.y);
-		minPos.z = std::min(v.z, minPos.z);
-
-		maxPos.x = std::max(v.x, maxPos.x);
-		maxPos.y = std::max(v.y, maxPos.y);
-		maxPos.z = std::max(v.z, maxPos.z);
-	}
-	return {minPos, maxPos};
 }
 
 
@@ -243,38 +275,21 @@ bool ObjImporter::import(const ObjectCollection& objCollection,
 	Object::merge(bodyGeometry, objs);
 
 
-	Vector3F64 minPos = bodyGeometry.vertices[0];
-	Vector3F64 maxPos = bodyGeometry.vertices[1];
-
-	for(const Vector3F64& v: bodyGeometry.vertices){
-		minPos.x = std::min(v.x, minPos.x);
-		minPos.y = std::min(v.y, minPos.y);
-		minPos.z = std::min(v.z, minPos.z);
-
-		maxPos.x = std::max(v.x, maxPos.x);
-		maxPos.y = std::max(v.y, maxPos.y);
-		maxPos.z = std::max(v.z, maxPos.z);
-	}
-
-	Vector3F64 delta = maxPos - minPos;
-	double maxDim = std::max(delta.x, std::max(delta.y, delta.z));
+	Size3F64 bounds = bodyGeometry.bounds();
+	Vector3F64 bodyOrigin = bounds.mid();
+	Vector3F64 bodySize = bounds.size();
+	double maxDim = std::max(bodySize.x, std::max(bodySize.y, bodySize.z));
 
 	double scaleFactor = 255.0 / maxDim;
 	if(importSettings.customScale.hasValue()){
 		scaleFactor = importSettings.customScale;
 	}
 
-	Bounds bounds{
-		.min = minPos,
-		.max = maxPos,
-	};
-	Vector3F64 bodyOrigin = bounds.origin();
-
 	bool isMechous = importSettings.objectMapping.values().contains(ObjectType::Wheel);
 
-
 	objectToC3D(bodyGeometry, m3d.body, importSettings, scaleFactor, bounds);
-	calcPhysics(m3d.body);
+//	calcPhysics(m3d.body);
+	calcPhysicsMtool(m3d.body);
 	if(importSettings.customVolume.hasValue()){
 		m3d.body.volume = importSettings.customVolume;
 	}
@@ -292,10 +307,7 @@ bool ObjImporter::import(const ObjectCollection& objCollection,
 	for(const Object& obj: objCollection.objects){
 		if(importSettings.objectMapping[obj.name] != ObjectType::BodyBound) continue;
 		boundFound = true;
-		Bounds b = objectBounds(obj);
-		objectToC3D(obj, m3d.bound, importSettings, scaleFactor, b);
-
-		calcPhysics(m3d.bound);
+		objectToC3D(obj, m3d.bound, importSettings, scaleFactor, obj.bounds());
 	}
 
 	if(!boundFound){
@@ -303,11 +315,15 @@ bool ObjImporter::import(const ObjectCollection& objCollection,
 		makeBoundingBox(bodyBoundsGeometry, bounds);
 		skinBodyBounds(bodyGeometry, bodyBoundsGeometry);
 		objectToC3D(bodyBoundsGeometry, m3d.bound, importSettings, scaleFactor, bounds);
-		alterBoundingBox(m3d.bound, m3d.body, isMechous);
-		calcPhysics(m3d.bound);
 	}
 
+	alterBoundingBox(m3d.bound, m3d.body, isMechous);
+	m3d.bound.rcm = m3d.body.rcm;
+	m3d.bound.J = m3d.body.J;
+	m3d.bound.volume = 0;
 
+	// TODO:
+//	calcPhysicsMtool(m3d.bound);
 
 	// TODO:
 	m3d.bodyColorOffset = 144;
@@ -317,27 +333,32 @@ bool ObjImporter::import(const ObjectCollection& objCollection,
 	m3d.bounds = m3d.body.maxBound;
 
 	for(const Object& obj: objCollection.objects){
-		ObjectType type = importSettings.objectMapping[obj.name] ;
+		ObjectType type = importSettings.objectMapping[obj.name];
+
 		if(type != ObjectType::WheelStatic &&
 		   type != ObjectType::Wheel) continue;
 
-		Bounds wheelBounds = objectBounds(obj);
+		Size3F64 wheelBounds = obj.bounds();
+		Vector3F64 wheelOrigin = wheelBounds.mid();
+		Vector3I32 wheelSize = Vector3I32::round(wheelBounds.size() * scaleFactor);
 
 		int32_t steer = type == ObjectType::Wheel ? 1 : 0;
 
 		Wheel wheel{
 			.steer = steer,
-			.f = (wheelBounds.origin() - bodyOrigin) * scaleFactor,
-			.width = (int32_t)((wheelBounds.max.x - wheelBounds.min.x) * scaleFactor),
-			.radius = (int32_t)((wheelBounds.max.y - wheelBounds.min.y) * scaleFactor),
+			.f = wheelOrigin * scaleFactor,
+			.width = wheelSize.x,
+			.radius = wheelSize.y,
 			.boundIndex = 0,
 			.model = {}
 		};
 
 		if(steer){
 			wheel.model.push_back({});
-			objectToC3D(obj, wheel.model[0], importSettings, scaleFactor, wheelBounds);
-			wheel.model[0].offsets = wheel.f;
+			Object centeredObj = obj;
+			centeredObj.move(wheelOrigin * -1);
+			objectToC3D(centeredObj, wheel.model[0], importSettings, scaleFactor, centeredObj.bounds());
+			wheel.model[0].offsets = Vector3I32::round(wheel.f);
 		}
 
 		m3d.wheels.push_back(wheel);
@@ -348,7 +369,7 @@ bool ObjImporter::import(const ObjectCollection& objCollection,
 	for(const Object& obj: objCollection.objects){
 		if(importSettings.objectMapping[obj.name] != ObjectType::DebreeBound) continue;
 
-		Bounds b = objectBounds(obj);
+		Size3F64 b = obj.bounds();
 		C3D debreeBounds;
 		objectToC3D(obj, debreeBounds, importSettings, scaleFactor, b);
 		calcPhysics(debreeBounds);
@@ -358,7 +379,7 @@ bool ObjImporter::import(const ObjectCollection& objCollection,
 	for(const Object& obj: objCollection.objects){
 		if(importSettings.objectMapping[obj.name] != ObjectType::Debree) continue;
 
-		Bounds b = objectBounds(obj);
+		Size3F64 b = obj.bounds();
 		C3D debree;
 		objectToC3D(obj, debree, importSettings, scaleFactor, b);
 
@@ -387,9 +408,9 @@ bool ObjImporter::import(const ObjectCollection& objCollection,
 		   type == ObjectType::Slot2 ||
 		   type == ObjectType::Slot3){
 
-			Bounds b = objectBounds(obj);
+			Size3F64 b = obj.bounds();
 			Slot slot {
-				.rSlots = (b.origin() - bodyOrigin) * scaleFactor,
+				.rSlots = Vector3I32::round(b.mid() * scaleFactor),
 				.locationAngleOfSlots = 0, //TODO:
 			};
 
@@ -468,27 +489,14 @@ void skinBodyBounds(const Object& body, Object& bounds){
 void alterBoundingBox(C3D& bounds, C3D& body, bool isMechous){
 
 	for(Polygon& polygon: bounds.polygons){
-		ColorId colorId = ColorId::DEFENSE;
-		const Vector3F32& v1 = bounds.vectices[polygon.indices[0].vertInd].tf;
-		const Vector3F32& v2 = bounds.vectices[polygon.indices[1].vertInd].tf;
-		const Vector3F32& v3 = bounds.vectices[polygon.indices[2].vertInd].tf;
-
-		Vector3F32 norm = (v2 - v1).cross(v2 - v3).normalized();
-		Normal flatNormal = {
-			.normal = (Vector3I8)(norm * 123),
-			.nPower = 8
-		};
-
-		Vector3F64 middle = {};
-		for(const PolygonIndex& index: polygon.indices) {
-			middle = middle + bounds.vectices[index.vertInd].tf;
-		}
-		middle = middle / 4;
-
+		const Vector3I8& flatNormal = polygon.flatNormal.normal;
 
 		if(isMechous) {
-			bool isLowerPlane = flatNormal.normal.x == 0 && flatNormal.normal.y == 0 && flatNormal.normal.z < 0;
-			colorId = isLowerPlane ? ColorId::ZERO_RESERVED : ColorId::BODY;
+			bool isLowerPlane = flatNormal.x == 0 && flatNormal.y == 0 && flatNormal.z < 0;
+//			colorId = isLowerPlane ? ColorId::ZERO_RESERVED : ColorId::BODY;
+			if(isLowerPlane) {
+				polygon.colorId = (uint32_t)ColorId::ZERO_RESERVED;
+			}
 
 			if(isLowerPlane) {
 				Vector3F32 maxAbsVertex = bounds.vectices[polygon.indices[0].vertInd].tf;
@@ -508,29 +516,23 @@ void alterBoundingBox(C3D& bounds, C3D& body, bool isMechous){
 					}
 				}
 
-				middle.x = body.maxBound.x;
-				middle.y = body.maxBound.y;
+				polygon.middle.x = body.maxBound.x;
+				polygon.middle.y = body.maxBound.y;
 				if(maxAbsVertex.x < 0) {
-					middle.x *= -1;
+					polygon.middle.x *= -1;
 				}
 
 				if(maxAbsVertex.y < 0) {
-					middle.y *= -1;
+					polygon.middle.y *= -1;
 				}
-				middle.z = body.minBound.z;
+				polygon.middle.z = body.minBound.z;
 			}
-			polygon.colorId = (uint32_t)colorId;
-			polygon.flatNormal = flatNormal;
-			polygon.middle = middle;
-
 		}
-
-
 	}
 }
 
-void makeBoundingBox(Object& result, const Bounds& bounds){
-	Vector3F64 mid = bounds.origin();
+void makeBoundingBox(Object& result, const Size3F64& bounds){
+	Vector3F64 mid = bounds.mid();
 	const Vector3F64& min = bounds.min;
 	const Vector3F64& max = bounds.max;
 	result.vertices = {
